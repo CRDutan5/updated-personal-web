e65f95604eaa3216a3833bfdfe64f7d5148af691
Carlitos <carlitosdutan@pursuit.org>
2025-01-13 12:47:23 -0500

    updated footer year

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="mt-14 bg-blue-600 py-14 text-center text-main border-t-4 border-blue-700">
       <p className="text-lg md:text-xl font-light">
-        &copy; 2024 Carlitos Dutan. All rights reserved.
+        &copy; 2025 Carlitos Dutan. All rights reserved.
       </p>
     </div>
   );
